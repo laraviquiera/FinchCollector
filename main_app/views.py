09474139b915bcbcb097e5efbe758200240a3330
username <lara.viquiera@gmail.com>
2024-01-31 14:29:02 -0800

    lab part 5 final

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from .models import Finch, Toy
 from .forms import FeedingForm
 
 # Define the home view
@@ -19,10 +19,13 @@ def finches_index(request):
   
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  id_list = finch.toys.all().values_list('id')
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
   feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
     'finch': finch,
-    'feeding_form': feeding_form
+    'feeding_form': feeding_form,
+    'toys': toys_finch_doesnt_have
     })
 
 def add_feeding(request, finch_id):
@@ -36,7 +39,7 @@ def add_feeding(request, finch_id):
   
 class FinchCreate(CreateView):
   model = Finch
-  fields = '__all__'
+  fields = ['name', 'species', 'description', 'age']
   
 class FinchUpdate(UpdateView):
   model = Finch
@@ -45,3 +48,11 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches'
+  
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
